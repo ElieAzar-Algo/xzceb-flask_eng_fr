054fdb7952a32a9590fc98233189d24beb85309c
ElieAzar-Algo <elie.azar3891.ea@gmail.com>
2023-05-15 01:23:06 +0300

    final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -7,37 +7,36 @@ load_dotenv()
 
 apikey = os.environ['apikey']
 url = os.environ['url']
-VERS = '2018-05-01'
 
 authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
-    version=VERS,
+    version='2018-05-01',
     authenticator=authenticator
 )
 
 language_translator.set_service_url(url)
 
 def englishToFrench(english_text=''):
-    """Function translating english text to french."""
+    """This function translates english to french."""
     if english_text != '':
-        response = language_translator.translate(
+        frenchtranslation = language_translator.translate(
             text=english_text,
-            model_id='en-fr').get_result()
-        french_text = response['translations'][0]['translation']
-        print(french_text)
-        return french_text
+            model_id='en-fr'
+        ).get_result()
+        
+        return frenchtranslation.get('translations')[0].get('translation')
     else:
         return 'Text should not be empty'
 
 
 def frenchToEnglish(french_text=''):
-    """Function translating french text to ensglish."""
+    """This function translates french to ensglish."""
     if french_text != '':
-        response = language_translator.translate(
+        ensglishtranslation = language_translator.translate(
             text=french_text,
-            model_id='fr-en').get_result()
-        english_text = response['translations'][0]['translation']
-        print(english_text)
-        return english_text
+            model_id='fr-en'
+        ).get_result()
+       
+        return ensglishtranslation.get("translations")[0].get("translation")
     else:
         return 'Text should not be empty'
